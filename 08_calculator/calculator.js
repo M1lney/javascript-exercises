d0d697394702b0960408f58cd999a52ca4eafc73
Tom Milne <tomj.milne@gmail.com>
2022-11-11 01:09:27 +1300

    completed two exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let amount = a;
+  for (let i = 0; i < b-1; i++) {
+    amount = amount * a;
+  }
+  return amount;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  if (a === 0) {
+    return 1;
+  }
+  for (let i = a; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const charArray = words.split(""); 
+    const areletters = charArray.filter(letter => {
+        return (letter.toUpperCase() != letter.toLowerCase()); 
+    }); 
+    for (let i = 0; i < areletters.length; i++) {
+        areletters[i] = areletters[i].toLowerCase();
+    }
+    const fowardStringJoined =  areletters.join("");
+    const reverse = areletters.reverse();
+    const reverseStringJoined = reverse.join("");
+    if (reverseStringJoined === fowardStringJoined) {
+        return true;
+    } else {
+        return false;
+    } 
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
